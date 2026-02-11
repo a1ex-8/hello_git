d75b2496e9d344a299aa8573b5b35c175e77d4a7
a1ex-8 <alexk550@mail.ru>
2026-02-12 01:39:22 +0700

    use my_funcs.py in hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+from my_funcs import add_two_number, diff_two_number
+
 a = "task_1"
 for i in range(10):
     print(f"{a}, {i=}")
@@ -6,4 +8,5 @@ for i in range(10):
 else:
     print("We finished this LOOP")
 
-
+print(f"{add_two_number()=}")
+print(f"{diff_two_number()=}")
