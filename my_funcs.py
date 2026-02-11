afcc975d84857d0902e27536b5a2f97890a4fbcc
a1ex-8 <alexk550@mail.ru>
2026-02-12 01:38:59 +0700

    add my_funcs.py

---
+++ my_funcs.py
@@ -0,0 +1,13 @@
+import random
+
+
+def add_two_number():
+    x = random.randint(a=1, b=100)
+    y = random.randint(a=1, b=100)
+    return x + y
+
+
+def diff_two_number():
+    x = random.randint(a=1, b=100)
+    y = random.randint(a=1, b=100)
+    return x - y
